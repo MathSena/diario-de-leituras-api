2acdfd19cfd9e45a28a98c346650f0ba37fbe351
MathSena <MathSena07@hotmail.com>
2025-07-19 21:00:23 -0300

    Initial commit

---
+++ src/main/java/com/mathsena/MyEntityResource.java
@@ -0,0 +1,7 @@
+package com.mathsena;
+
+import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+
+public interface MyEntityResource extends PanacheEntityResource<MyEntity, Long> {
+
+}
---
+++ src/main/java/com/mathsena/model/Status.java
@@ -0,0 +1,4 @@
+package com.mathsena.model;
+
+public enum Status {
+}
